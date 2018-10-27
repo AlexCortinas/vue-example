c599e0379527f8752b2a15fde152cf6009c47d64
Álex Cortiñas <mail@alexcortinas.es>
2018-10-27 12:19:03 +0200

    Añadiendo plugin para mostrar alertas

--- src/app/App.vue
+++ src/app/App.vue
@@ -1,5 +1,9 @@
 <template>
   <div id="app">
+    <notifications
+      :max="3"
+      :width="400"
+      position="top center"/>
     <MenuBar/>
     <router-view class="content"/>
   </div>
@@ -16,5 +20,27 @@ export default {
 <style scoped lang="scss">
   #app {
     color: #56b983;
+  }
+
+  .notification {
+    margin: 10px;
+    margin-bottom: 0;
+    border-radius: 3px;
+    font-size: 13px;
+    padding: 10px 20px;
+    color: #495061;
+    background: #EAF4FE;
+    border: 1px solid #D4E8FD;
+
+    .error {
+      background: #000000;
+    }
+
+    .notification-title {
+      letter-spacing: 1px;
+      text-transform: uppercase;
+      font-size: 10px;
+      color: #2589F3;
+    }
   }
 </style>
