8c454c389a73b3d4184eac24f4255c9a1692acb3
Álex Cortiñas <mail@alexcortinas.es>
2018-10-27 12:30:53 +0200

    Gestión de la autenticación
    Implementado almacén para guardar los datos del usuario actual y los
    métodos relativos a la autenticación.
    También se han implementado interceptores para que las peticiones vayan
    con el JsonWebToken en caso de que haya uno.
    El almacén se carga en la App como datos para que quede "registrado"
    En el created del App también se comprueba si hay un token almacenado,
    y de haberlo se lanza la petición para autenticarse

--- src/app/App.vue
+++ src/app/App.vue
@@ -10,10 +10,26 @@
 </template>
 
 <script>
+import auth from './common/auth'
 import MenuBar from './components/MenuBar'
 
 export default {
-  components: { MenuBar }
+  name: 'App',
+  components: { MenuBar },
+  data() {
+    return {
+      // enlazamos el objeto donde vamos a guardar los datos de autenticación
+      // de esta forma se activa el data-binding y los podemos usar como
+      // propiedades públicas en los componentes
+      storeAuth: auth
+    }
+  },
+  created() {
+    // nada más ejecutar la aplicación comprobamos si estamos autenticados
+    if (auth.getToken()) {
+      auth.authenticate()
+    }
+  }
 }
 </script>
 
